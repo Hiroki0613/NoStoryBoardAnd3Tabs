4fe7eea40a9761bd06b08c44f618fe9e71b490a7
Hiroki Kondo <kondo.matr02@gmail.com>
2020-02-08 13:55:04 +0900

    Initial Commit

---
+++ NoStoryBoardAnd3Tabs/HappyViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NoStoryBoardAnd3Tabs
+//
+//  Created by 近藤宏輝 on 2020/02/08.
+//  Copyright © 2020 Hiroki. All rights reserved.
+//
+
+import UIKit
+
+class HappyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
